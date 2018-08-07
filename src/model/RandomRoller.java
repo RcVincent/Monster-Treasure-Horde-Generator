47f14284c64962f22bde31b98e98f3d50760f9db
Ryan <rvincent@ycp.edu>
2018-08-06 21:38:21 -0400

    started work on the random roller and added a blank main class

--- src/model/RandomRoller.java
+++ src/model/RandomRoller.java
@@ -1,5 +1,21 @@
 package model;
 
+import java.util.Random;
 public class RandomRoller {
+	private Random r;
+	 
+	//for the gm to decide 
+	
+	 
+	
+	
+	public Random getR() {
+		return r;
+	}
 
+	public void setR(Random r) {
+		this.r = r;
+	} 
+	
+	 
 }
